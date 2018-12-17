1310687b25a5bdbd7d47a035f44a0ae4dc167e9d
Heath Dunlop <heath.dunlop.hd@gmail.com>
2018-12-18 09:33:50 +1300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nomos-dashboard-client/build/index.html/precache-manifest.ceb14498aa1aec6dcc132bbd73ec1188.js"
+  "/precache-manifest.7c10dd8dccadeba2990bde2e7f078f41.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/nomos-dashboard-client/build/index.html/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
